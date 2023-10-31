d33d19e5e3acc455b7e13a94e42f8cc77a250fa4
Shubham Shandilya <128954805+ShubhamShandilya0309@users.noreply.github.com>
2023-10-31 11:13:55 +0000

    let,var and const done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,10 @@
 console.log("Shubham")
+const accountId =144553
+let accountEmail ="shubham@google.com"
+var accountPswd= "12345"
+accountCity= "New Delhi"
+//accountId=2; // not allowed
+accountEmail="sshss@gmail.com"
+accountPswd="2345"
+accountCity ="_Delhi"
+console.table([accountId,accountEmail,accountPswd,accountCity]);
