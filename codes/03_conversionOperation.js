81f508a57a674711d34ac6a183d8a98533a4bb05
Shubham Shandilya <128954805+ShubhamShandilya0309@users.noreply.github.com>
2023-11-01 10:58:06 +0000

    datatype conversion

---
+++ codes/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score="33"
+console.log(typeof score)
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+//now try with a string
+let scr="ABC"
+console.log(typeof scr)
+let val=Number(scr);
+console.log(typeof val)
+console.log(val)
+// for null it gives 0 and for undefined gives NaN
+/*Output :
+string
+number
+33
+string
+number
+NaN : Not A Number
+ */
