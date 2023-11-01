f83db141528f2de90afe08e3b1757fca6075dfc2
Shubham Shandilya <128954805+ShubhamShandilya0309@users.noreply.github.com>
2023-11-01 11:27:44 +0000

    datatype summary

---
+++ codes/05_datatypesSummary.js
@@ -0,0 +1,7 @@
+//https://tc39.es/ecma262/ : Important Link
+//primitive data types
+// call by value 
+/* 7 types : string , number , boolean , null , undefined , symbol , bigint */
+// refernce data types : non-primitive
+// array ,objects , functions
+//Is Js a statically typed or dynamically typed?
