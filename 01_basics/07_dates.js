caf7d76636ee7118c9186bbc1f3400ef8e183360
Shubham Shandilya <128954805+ShubhamShandilya0309@users.noreply.github.com>
2024-01-04 09:03:25 +0000

    04thJanuary'23

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -1,10 +1,21 @@
-let myDate = new Date()
+//let myDate = new Date()
 // console.log(myDate);
 // console.log(myDate.toDateString());
 // console.log(myDate.toISOString());
 // console.log(typeof myDate);
 
-let myCreatedDate= new Date("2023-11-02")
-console.log(myCreatedDate.toLocaleDateString());
-let myTimeStamp=Date.now()
-console.log(Math.floor(myTimeStamp/1000));
+// let myCreatedDate= new Date("2023-11-02")
+// console.log(myCreatedDate.toLocaleDateString());
+// let myTimeStamp=Date.now()
+// console.log(Math.floor(myTimeStamp/1000));
+let myDate=new Date();
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toISOString());
+// console.log(typeof(myDate));
+// console.log(myDate.getDate());
+// console.log(myDate.getMonth());
+// console.log(myDate.getDay());
+console.log(`${myDate.getDate()}/${myDate.getMonth()+1}/${myDate.getFullYear()}`);
+//toLocaleString is the most important one
--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -15,7 +15,11 @@ const allNewHeros=[...marvel, ...dc]
 
 const anotherArray=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
 const real_anotherArray=anotherArray.flat(Infinity)
-console.log(anotherArray);
-console.log(real_anotherArray);
-
+// console.log(anotherArray);
+// console.log(real_anotherArray);
+console.log(Array.isArray("SHUBHAM"));
+console.log(Array.from("SHUBHAM"));
 console.log("It give an empty array ",Array.from({name :"Shubham"}))
+//interesting
+let s1=100,s2=200,s3=300;
+console.log(Array.of(s1,s2,s3));
